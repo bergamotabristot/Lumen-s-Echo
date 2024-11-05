07967ded3d3df14d0cc3e90380cfa013329f0fa1
Pedro Henrique Bristot San Thiago <psaothiago3@gmail.com>
2024-11-04 21:28:34 -0300

    Update gameScene.js

--- mygame/src/scenes/gameScene.js
+++ mygame/src/scenes/gameScene.js
@@ -61,7 +61,7 @@ k.onButtonRelease("jump", () => {
 
 // Adiciona plataformas ao jogo
 k.add([
-    k.rect(4000, 20),
+    k.rect(400, 20),
     k.pos(50, 300),
     k.body({ isStatic: true }),
     k.color(5, 5, 5),
